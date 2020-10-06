ac74de54de71c6c73fb7cfa1fee2110825fb1f1c
Eriq <55072762+athoriq1224@users.noreply.github.com>
2020-10-06 14:07:35 +0700

    ApiService.kt
    Comment

--- app/src/main/java/com/example/covid19/network/ApiService.kt
+++ app/src/main/java/com/example/covid19/network/ApiService.kt
@@ -10,15 +10,19 @@ import retrofit2.http.GET
 import retrofit2.http.Url
 import java.util.concurrent.TimeUnit
 
+//untuk meng-get data yg ada di dlam directory summray
 interface ApiService {
     @GET("summary")
     fun getAllNegara() : Call<ResponseCountry>
 }
+//untuk akses tabel yg tersedia di model
 interface InfoService{
     @GET
     fun getInfoService(@Url url:String?) : Call<List<InfoNegara>>
 }
+//untuk koneksi
 object RetrofitBuilder{
+        //untuk mengcek koneksi internet
     private val okhttp = OkHttpClient().newBuilder()
         .connectTimeout(15,TimeUnit.SECONDS)
         .readTimeout(10,TimeUnit.SECONDS)
